165e97bef77527ae0dead6be098ef28e42dc18aa
Nabeel Zewail <nzewail@hey.com>
2021-10-15 10:56:38 -0700

    make api calls async

--- main.py
+++ main.py
@@ -1,73 +1,45 @@
 from random import randint
-from typing import List, Tuple
-
+from datetime import date
+import aiohttp
+import asyncio
+import time
 import pandas as pd
-import requests
-import streamlit as st
+import pandas as pd
 from bokeh.models import ColumnDataSource, Legend
 from bokeh.models.tools import HoverTool
 from bokeh.plotting import figure
+from typing import Tuple
+import requests
+
+import streamlit as st
+
+start_time = time.time()
+
 
 HOST = "https://ergast.com/api/f1"
 CHAMPIONSHIPS = frozenset({"DriverStandings", "ConstructorStandings"})
 FIRST_F1_SEASON = 1950
+CURRENT_F1_SEASON = date.today().year
 PAGE_NAME = "F1 Season Tracker"
-
-
-def hit_url(url: str):
-    res = requests.get(url)
-    if res.status_code == 200:
-        return res.json()
-    raise Exception(
-        "%s returned an unsuccessful status_code: %s %s"
-        % (url, res.status_code, res.text)
-    )
 
 
 def build_url_base(season) -> str:
     return f"{HOST}/{season}"
 
 
-def standings_url(season: int, race_round: int, championship: str) -> str:
-    if championship not in CHAMPIONSHIPS:
-        raise Exception("%s must be in %s" % (championship, CHAMPIONSHIPS))
-    return f"{build_url_base(season)}/{race_round}/{championship}.json"
-
-
+@st.cache(show_spinner=False)
 def get_season_num_rounds(season="current") -> Tuple[int, int]:
-    url = f"{build_url_base(season)}/last/results.json"
-    response = hit_url(url)
+    url = f"{build_url_base(season)}/last.json"
+    response = requests.get(url).json()
     race_table = response["MRData"]["RaceTable"]
     return int(race_table["season"]), int(race_table["round"])
-
-
-@st.cache(show_spinner=False)
-def hit_standings_api(season: int, race_round: int, championship: str) -> List[dict]:
-    url = standings_url(season, race_round, championship)
-    results = hit_url(url)
-    return parse_standings_response(race_round, championship, results)
-
-
-def get_standings(season: int, race_round: int, championship: str) -> List[dict]:
-    standings = []
-    latest_iteration = st.empty()
-    progress_bar = st.progress(0)
-
-    for race in range(1, race_round + 1, 1):
-        latest_iteration.text(f"Fetching data for round {race}")
-        progress_bar.progress(race / (race_round + 1))
-        parsed_response = hit_standings_api(season, race, championship)
-        standings.extend(parsed_response)
-    latest_iteration.empty()
-    progress_bar.empty()
-    return standings
 
 
 def clean_championship_type(championship: str) -> str:
     return championship[:-9]
 
 
-def parse_standings_response(race_num, championship, response):
+def parse_standings_response(response, race_num, championship):
     standings = response["MRData"]["StandingsTable"]["StandingsLists"][0][championship]
     parsed_response = []
     championship_type = clean_championship_type(championship)
@@ -77,43 +49,25 @@ def parse_standings_response(race_num, championship, response):
             "race_num": race_num,
             "position": int(team["position"]),
             championship_type: str(team[championship_type][team_id].capitalize()),
-            "points": int(team["points"]),
+            "points": float(team["points"]),
         }
         parsed_response.append(standing)
     return parsed_response
 
 
-def main():
-    st.set_page_config(
-        page_title=PAGE_NAME,
-    )
-    st.title(PAGE_NAME)
-    st.markdown("This data is courtesy of [Ergast](http://ergast.com/mrd/)")
+async def get_standings(session, url, race_num, championship):
+    async with session.get(url) as resp:
+        pokemon = await resp.json()
+        return parse_standings_response(pokemon, race_num, championship)
 
-    season, _ = get_season_num_rounds()
 
-    season_dropdown = st.sidebar.slider(
-        "Season", min_value=FIRST_F1_SEASON, max_value=season, step=1, value=season
-    )
-
-    _, num_races = get_season_num_rounds(season_dropdown)
-
-    round_slider = st.sidebar.slider(
-        "Race Round",
-        min_value=1,
-        max_value=num_races,
-        step=1,
-        value=num_races,
-    )
-    standings_type = st.sidebar.radio("Championship", options=CHAMPIONSHIPS, index=1)
-
-    standings = get_standings(season_dropdown, round_slider, standings_type)
-    title = clean_championship_type(standings_type)
-
-    df = pd.DataFrame.from_records(standings)
+def render(results, championship, race_round, season):
+    df = pd.DataFrame.from_records(results)
+    title = clean_championship_type(championship)
+    st.write(title)
 
     plot = figure(
-        title=f"{season_dropdown} F1 {title.capitalize()} Standings after Round {round_slider}",
+        title=f"{season} F1 {title.capitalize()} Standings after Round {race_round}",
         x_axis_label="Race Number",
         y_axis_label="Number of Points",
         tools=[HoverTool(), "save"],
@@ -142,10 +96,10 @@ def main():
             x="races", y="points", line_width=2, line_color=color, source=source
         )
         plr = team_df.iloc[team_df["race_num"].argmax()]["points"]
-        points_last_race = int(plr)
+        points_last_race = float(plr)
         legend_it.append((f"{team}\t{points_last_race}", [line]))
 
-    legend_it.sort(reverse=True, key=lambda x: int(x[0].split("\t")[-1]))
+    legend_it.sort(reverse=True, key=lambda x: float(x[0].split("\t")[-1]))
     legend = Legend(items=legend_it)
     legend.click_policy = "hide"
     legend.title = title.capitalize()
@@ -155,5 +109,44 @@ def main():
     st.bokeh_chart(plot, use_container_width=True)
 
 
-if __name__ == "__main__":
-    main()
+async def main():
+    st.set_page_config(
+        page_title=PAGE_NAME,
+    )
+    st.title(PAGE_NAME)
+    st.markdown("This data is courtesy of [Ergast](http://ergast.com/mrd/)")
+
+    season = st.sidebar.slider(
+        "Season", min_value=FIRST_F1_SEASON, max_value=CURRENT_F1_SEASON, step=1, value=CURRENT_F1_SEASON
+    )
+
+    _, num_races = get_season_num_rounds(season)
+
+    race_round = st.sidebar.slider(
+        "Race Round",
+        min_value=1,
+        max_value=num_races,
+        step=1,
+        value=num_races,
+    )
+    championship = st.sidebar.radio("Championship", options=CHAMPIONSHIPS, index=1)
+
+    async with aiohttp.ClientSession() as session:
+
+        tasks = []
+        for number in range(1, race_round):
+            url = f'https://ergast.com/api/f1/{season}/{number}/{championship}.json'
+            tasks.append(asyncio.ensure_future(get_standings(session, url, number, championship)))
+
+        start_time = time.time()
+        original_standings = await asyncio.gather(*tasks)
+        print("--- %s seconds ---" % (time.time() - start_time))
+
+    standings = []
+    for standing in original_standings:
+        standings.extend(standing)
+    render(standings, championship, race_round, season)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
